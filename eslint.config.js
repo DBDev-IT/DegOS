5b67711dd06a11ecde99de70fc3fba527b4af266
Damir <dambul2809@mail.ru>
2026-05-19 14:06:49 +0000

    deploy.yml, eslint.config.js, package.json: Create deploy workflow and fix formatting

--- eslint.config.js
+++ eslint.config.js
@@ -4,35 +4,33 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import prettier from "eslint-plugin-prettier/recommended";
 
-export default js.config(
-	{ ignores: ["dist"] },
+export default [
 	{
-		extends: [js.configs.recommended, prettier],
-		files: ["**/*.{json,js,jsx,css,md}"],
+		ignores: ["dist"]
+	},
+	{
+		files: ["**/*.{js,jsx}"],
 		languageOptions: {
 			ecmaVersion: 2020,
-			globals: globals.browser
+			globals: globals.browser,
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true
+				}
+			}
 		},
 		plugins: {
 			"react-hooks": reactHooks,
 			"react-refresh": reactRefresh
 		},
 		rules: {
+			...js.configs.recommended.rules,
 			...reactHooks.configs.recommended.rules,
 			"react-refresh/only-export-components": [
 				"warn",
 				{ allowConstantExport: true }
-			],
-			"prettier/prettier": [
-				"error",
-				{
-					semi: true,
-					singleQuote: false,
-					trailingComma: "none",
-					tabWidth: 4,
-					useTabs: true
-				}
 			]
 		}
-	}
-);
+	},
+	prettier
+];
